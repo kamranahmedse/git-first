60b5891d302527808048d474789183150195bc31
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-11-02 03:03:58 +0400

    Add logic for app

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function openFirstCommit() {
-    // @todo
-}
+import repo from 'repo-first-commit';
 
 chrome.contextMenus.create({
-    title: 'Initial Commit',
-    documentUrlPatterns: ['*://github.com/*/*'],
-    contexts: ['all'],
-    onclick: openFirstCommit
+  title: 'Initial Commit',
+  documentUrlPatterns: ['*://github.com/*/*'],
+  contexts: ['all'],
+  onclick: (info, tab) => {
+    // Get the owner, repo and branch name from the URL
+    const pageUrl = info.pageUrl.replace(/.+:\/\/(www\.)?github.com\/?/, '');
+    const urlParts = pageUrl.split('/');
+
+    repo({
+      owner: urlParts[0],
+      repo: urlParts[1],
+      sha: urlParts[3]
+    }).then(commit => {
+      chrome.tabs.update(tab.id, {
+        url: commit.html_url
+      });
+    }).catch(err => {
+      alert(err.message);
+    });
+  }
 });
