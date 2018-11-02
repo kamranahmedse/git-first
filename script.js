dc6a5af2bb852b90172f9c07dbec02cab668fae0
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-11-02 14:14:31 +0400

    Refactor commit loader

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-import repo from 'repo-first-commit';
+import fetchFirstCommit from 'repo-first-commit';
 
+/**
+ * Loads and redirects user to first commit
+ * @param info
+ * @param tab
+ */
+const loadFirstCommit = (info, tab) => {
+  // Remove scheme and host from the URL
+  const pageUrl = info.pageUrl.replace(/.+:\/\/(www\.)?github.com\/?/, '');
+  const [owner, repo, , sha] = pageUrl.split('/');
+
+  // Fetch the first commit
+  fetchFirstCommit({ owner, repo, sha })
+    .then(commit => {
+      chrome.tabs.update(tab.id, {
+        url: commit.html_url
+      });
+    })
+    .catch(err => {
+      alert(err.message);
+    });
+};
+
+// Register the context menu item
 chrome.contextMenus.create({
   title: 'Initial Commit',
   documentUrlPatterns: ['*://github.com/*/*'],
   contexts: ['all'],
-  onclick: (info, tab) => {
-    // Get the owner, repo and branch name from the URL
-    const pageUrl = info.pageUrl.replace(/.+:\/\/(www\.)?github.com\/?/, '');
-    const urlParts = pageUrl.split('/');
-
-    repo({
-      owner: urlParts[0],
-      repo: urlParts[1],
-      sha: urlParts[3]
-    }).then(commit => {
-      chrome.tabs.update(tab.id, {
-        url: commit.html_url
-      });
-    }).catch(err => {
-      alert(err.message);
-    });
-  }
+  onclick: loadFirstCommit
 });
